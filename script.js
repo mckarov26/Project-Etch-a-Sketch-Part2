fe6966c66708f0ec90082c4867a4eb012dd081dd
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-01-23 20:50:49 +1300

    Refactor the code and Added a new function.
    -Refactoring the codes from buttonSize event handler
    -Added a new function declaration and place the code for getting the grid-size using prompt.
    -To improved readability and structure of the code.

--- script.js
+++ script.js
@@ -10,20 +10,7 @@ gridSize = document.querySelector("#size");
 
 //Add Event Listener to display the number of squares per side.
 buttonSize.addEventListener("click", () => {
-   let size = prompt("Choose 1-100 for the number of squares per side for the new grid");
-    size = Number(size)
-    if ( !isNaN(size) && size > 0 && size < 100) {
-        gridInfo.textContent = `The layout of your grid is:`;
-        gridSize.style["font-weight"] = "bold";
-        gridSize.style["font-size"] = "1.5rem"
-        gridSize.textContent = ` ${size} x ${size} squares`;
-        
-    } else {
-        alert("Please choose a valid number from 1-100 only!");
-    }
-
-
-
+    getSize();
 })
 
 //Function to get random color for the grids.
@@ -38,4 +25,19 @@ function getRandomColor() {
 //Function to get black color for the grids.
 function getBlackColor() {
     return rgb(0, 0, 0);
+}
+
+//Function to get the size of grid from the user using prompt and to display the grid-size. 
+function getSize() {
+    let size = prompt("Choose 1-100 for the number of squares per side for the new grid");
+    size = Number(size)
+    if ( !isNaN(size) && size > 0 && size < 100) {
+        gridInfo.textContent = `The layout of your grid is:`;
+        gridSize.style["font-weight"] = "bold";
+        gridSize.style["font-size"] = "1.5rem"
+        gridSize.textContent = ` ${size} x ${size} squares`;
+        
+    } else {
+        alert("Please choose a valid number from 1-100 only!");
+    }
 }
